4c6c3ec013f7c76a7d1b1cb6c4bc49dbc8352420
chrisperez626 <chrisperez626@gmail.com>
2018-03-21 21:35:26 -0400

    initial program files

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+var minutes = 1;
+var seconds = 60;
+
+var intervalId;
+
+function timeGo(){
+    clearInterval(intervalId);
+    intervalId = setInterval(timeDecrease, 1000);
+}
+
+function testScorer(){
+    if(d){
+
+    }
+}
+
+
+function timeDecrease(){
+    minutes = 0;
+    
+    seconds--;
+
+    $("#Timer").html("<h1>" + "Time left: " + seconds + "</h1>")
+
+    if(seconds === 0){
+        timeUp();
+
+        minutes = 1;
+
+        seconds = 1;
+    }
+
+}
+
+function timeUp(){
+    clearInterval(intervalId);
+}
+
+timeGo();
