d4fe43a7e6768013266a52d6160b9f5dbb1cc0a7
chrisperez626 <chrisperez626@gmail.com>
2018-03-22 22:05:57 -0400

    initial program files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,17 @@
-var minutes = 1;
-var seconds = 60;
+//variables preset
 
+var seconds = 60;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
 var intervalId;
 
+//function to have the clock rundown
 function timeGo(){
     clearInterval(intervalId);
     intervalId = setInterval(timeDecrease, 1000);
 }
 
-function testScorer(){
-    if(d){
-
-    }
-}
-
-
+//function to have seconds decrease
 function timeDecrease(){
     minutes = 0;
     
@@ -25,17 +22,36 @@ function timeDecrease(){
     if(seconds === 0){
         timeUp();
 
-        minutes = 1;
-
         seconds = 60;
     }
 
 }
 
+//function for when timer runs up to stop game
+//shows user how many correct and incorrect answers
 function timeUp(){
     clearInterval(intervalId);
+    alert("Times Up!!");
+    alert("Correct: " + correctAnswers);
+    alert("Incorrect: " + incorrectAnswers);
 }
 
-$("#submitButton").click(function(){
-    console.log("nigger")
+//Runs clock
+timeGo();
+
+//adds correct answer to score
+$("body").on("click",".correctAnswer",function(){
+    correctAnswers++;
 })
+
+//adds incorrect answer to score
+$("body").on("click",".incorrect",function(){
+    incorrectAnswers++;
+})
+
+//if user finishes early this allows them to submit quiz
+//stops clock and shows results
+$("body").on("click","button",function(){
+    timeUp();
+})
+
